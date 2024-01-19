a02d39f67fc214074d4f118b72b117520e504556
Lingxi Li <jsnjllx@163.com>
2024-01-19 23:37:41 +0800

    test

---
+++ format.py
@@ -0,0 +1,25 @@
+age = 20
+name = 'Swaroop'
+
+print('{0} was {1} years old when he wrote this book'.format(name, age))
+print('Why is {0} playing with that python?'.format(name))
+print(name + ' was ' + str(age) +' years old ')
+
+print('{name} was {age} years old when he wrote this book'.format(name = name, age = age))
+print('{} was {} years old when he wrote this book'.format(name, age))
+print(f'{name} was {age} years old when he wrote this book')
+print('{:.5f}'.format(1.0/3))
+print('{:_^7}'.format("hello"))
+print('a', end=' ')
+print('b', end=' ')
+print("\n")
+
+print("what's your name ?")
+print('hello " hello')
+print('what\'s your name ?')
+print("hello \" hello")
+
+print(r"hello \n")
+print("\\1")
+print(r"\1")
+
---
+++ variables.py
@@ -0,0 +1,41 @@
+a_number = 67
+if isinstance(a_number, int):
+    print("{}".format(a_number))
+
+s = '''This is a multi line sentences.
+There are many words in it'''
+
+print(s)
+
+print(a_number)
+
+s = 'This is a string. \
+This continues the string.'
+
+print(s)
+
+s = "This is a double quoted string. \
+This continues the string."
+print(s)
+
+s = ("this"
+     " is")
+print(s)
+
+s = ["this"
+     " is"]
+print(s)
+
+s = {"this"
+" is"}
+
+string_s = ''
+
+for i in s:
+    string_s += i
+
+print(string_s)
+
+'''book mark
+Indentation
+'''
